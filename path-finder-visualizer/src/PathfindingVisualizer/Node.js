695dca0164948ce70c9cb907f777ef000d0eb88b
gulycat1214 <guillermo.cobo1998@gmail.com>
2021-01-26 13:24:25 +0100

    Adding walls to grid + dijkstra not tested yet

--- path-finder-visualizer/src/PathfindingVisualizer/Node.js
+++ path-finder-visualizer/src/PathfindingVisualizer/Node.js
@@ -2,19 +2,14 @@ import React, {useState} from 'react';
 import './Node.css';
 export default function Node (props) {
 
-    const {isFinish, isStart,isWall} = props;
-    const [extraClassName, setExtraClassName] = useState(isFinish ? 'node-finish' : isStart ? 'node-start' : isWall ? 'node-is-wall' : '');
-
-    const addWall = () => {
-        setExtraClassName('node-is-wall')
-    }
-
+    const {row, col, isFinish, isStart, isWall, addWall} = props;
+    const extraClassName = isFinish ? 'node-finish' : isStart ? 'node-start' : isWall ? 'node-is-wall' : '';
 
     return(
         <div 
             className={`node ${extraClassName}`}
             onClick={() => {
-               addWall();
+                addWall(row, col);
             }}
         >
         </div>
--- path-finder-visualizer/src/PathfindingVisualizer/PathfindingVisualizer.js
+++ path-finder-visualizer/src/PathfindingVisualizer/PathfindingVisualizer.js
@@ -5,6 +5,11 @@ export default function PathfindingVisualizer(){
 
     const [nodes, setNodes] = useState([]);
 
+    const handleMouseEnter = (row, col) => {
+        const newGrid = addOrDeleteWall(nodes, row, col);
+        setNodes(newGrid);
+    }
+
     useEffect(() => {
         let nodes = [];
         for( let row = 0; row < 15; row++) {
@@ -15,7 +20,10 @@ export default function PathfindingVisualizer(){
                     col,
                     isStart: row === 0 && col === 0,
                     isFinish: row === 14 && col === 49,
-                    isWall: false
+                    isWall: false,
+                    isVisited: false,
+                    distance: (row === 0 && col === 0) ? 0 : Infinity,
+                    previousNode: null
                 }
                 currentRow.push(currentNode);
             }
@@ -26,17 +34,22 @@ export default function PathfindingVisualizer(){
     }, []);
 
     return(
+        <>
         <div className="grid">
             {nodes.map((row, rowIndex) => {
                 return(
                     <div>
                         {row.map((node, nodeIndex) => {
-                            const {isStart, isFinish} = node;
+                            const {row, col, isStart, isFinish, isWall} = node;
                             return(
                                 <Node
+                                    row={row}
+                                    col={col}
                                     key={nodeIndex}
                                     isStart={isStart}
                                     isFinish={isFinish}
+                                    isWall={isWall}
+                                    addWall={(row, col) => handleMouseEnter(row, col)}
                                 ></Node>
                             )
                         })}
@@ -44,5 +57,22 @@ export default function PathfindingVisualizer(){
                 )
             })}
         </div>
+        <div className="button">
+            <button
+                type="button"
+            >Dijkstra</button>
+        </div>
+        </>
     );
+}
+
+const addOrDeleteWall = (grid, row, col) => {
+    const newGrid = grid.slice();
+    const node = newGrid[row][col];
+    const newNode = {
+        ...node,
+        isWall: !node.isWall,
+    };
+    newGrid[row][col] = newNode;
+    return newGrid;
 }
